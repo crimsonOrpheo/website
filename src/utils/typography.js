cc26cfaa06c88a875dba1ee4d22b565d4bfde874
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-05-25 21:34:50 +0200

    Changing baseLineHeight

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from 'typography'
 import githubTheme from 'typography-theme-github'
 
-githubTheme.baseLineHeight = 1.8
+githubTheme.baseLineHeight = 1.5
 githubTheme.scaleRatio = 2
 githubTheme.baseParagraphSpacing = 24
 
