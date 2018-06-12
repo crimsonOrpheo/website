432a87baaa386af199a8bbd71906adb9307b27a0
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-06-12 17:08:45 +0200

    handling scrolling for search modal

--- src/components/search/VerticalNavigationList.js
+++ src/components/search/VerticalNavigationList.js
@@ -3,6 +3,8 @@ import NavigationItem from './NavigationItem';
 import Search from './Search';
 import '../../styles/animations.css'
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+
 
 class VerticalNavigationList extends Component {
   constructor(...rest) {
@@ -11,52 +13,53 @@ class VerticalNavigationList extends Component {
       hits: null,
     };
   }
+
   render() {
     const { currentSlug, edges, searchData } = this.props;
     const { hits } = this.state;
     return (
-      <div
-        style={{
-          backgroundColor : 'rgba(0, 0, 0, 0.85)',
-          color: 'White',
-          zindex: 1000,
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0,
-        }}>
-        <Search
-          data={searchData}
-          onSearch={(text, hits) =>
-            this.setState({
-              hits: text !== '' ? hits : null,
-            })
-          }
-        />
-        <nav>
-          <dl>
-            {edges
-              .filter(
-                ({ node }) =>
-                  !hits || hits.filter(hit => hit.id === node.id).length > 0,
-              )
-              .map(({ node }, index) => (
-                <div key={`nav-header-wrapper-${index}`}>
-                  <NavigationItem
-                    depth={1}
-                    value={node.frontmatter.title}
-                    extract={node.excerpt}
-                    key={`nav-item-${index}-${node.fields.slug}`}
-                    href={`${node.fields.slug}`}
-                    tags={node.frontmatter.tags}
-                    date={node.frontmatter.date}
-                  />
-                </div>
-              ))}
-          </dl>
-        </nav>
-      </div>
+        <div
+          style={{
+            backgroundColor : 'rgba(0, 0, 0, 0.85)',
+            color: 'White',
+            zindex: 1000,
+            position: 'fixed',
+            left: '0',
+            right: '0',
+            top: '0',
+            bottom: '0',
+          }}>
+          <Search
+            data={searchData}
+            onSearch={(text, hits) =>
+              this.setState({
+                hits: text !== '' ? hits : null,
+              })
+            }
+          />
+          <nav>
+            <dl>
+              {edges
+                .filter(
+                  ({ node }) =>
+                    !hits || hits.filter(hit => hit.id === node.id).length > 0,
+                )
+                .map(({ node }, index) => (
+                  <div key={`nav-header-wrapper-${index}`}>
+                    <NavigationItem
+                      depth={1}
+                      value={node.frontmatter.title}
+                      extract={node.excerpt}
+                      key={`nav-item-${index}-${node.fields.slug}`}
+                      href={`${node.fields.slug}`}
+                      tags={node.frontmatter.tags}
+                      date={node.frontmatter.date}
+                    />
+                  </div>
+                ))}
+            </dl>
+          </nav>
+        </div>
     );
   }
 }
