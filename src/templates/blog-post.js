2f219124863520c6cbc84f69db46f016de471303
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-06-14 17:34:49 +0200

    update opengraph

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -30,7 +30,7 @@ class BlogPostTemplate extends React.Component {
           <meta name="twitter:creator" content="@BovetJulien" />
           <meta property="og:url" content="https://julienbovet.com/{node.fields.slug}" />
           <meta property="og:title" content={post.frontmatter.title} />
-          <meta property="og:type" content="website" />
+          <meta property="og:type" content="article" />
           <meta property="og:description" content={post.excerpt} />
         </Helmet>
         <h1>{post.frontmatter.title}</h1>
