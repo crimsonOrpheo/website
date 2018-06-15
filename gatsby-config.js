dfd898688ea49376a631116a32dc0804db52f1c6
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-06-15 15:44:21 +0200

    Adding sitemap.xml

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,9 @@ module.exports = {
   pathPrefix: '/',
   plugins: [
     {
+      resolve: `gatsby-plugin-sitemap`
+    },
+    {
       resolve: `gatsby-source-filesystem`,
         options: {
           path: `${__dirname}/src/pages`,
