8ec453cde0f31405a71413fa9c7ba7ddc72d2d08
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-08-24 10:29:42 +0200

    changing bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -32,7 +32,7 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          I flip burgers, beat the drums & crunch <a href="/tags/data">data</a>.
+          I beat the drums & crunch <a href="/tags/data">data</a>.
         <br/>
           <a href="https://twitter.com/BovetJulien">
             <img
