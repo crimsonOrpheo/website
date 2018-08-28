5abd744ffa7587ee7fab6bdfc5c77e6bcb6c0b96
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-08-28 16:09:00 +0200

    Adding tracking to tag pills.

--- src/components/Taglist.js
+++ src/components/Taglist.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import '../styles/tagPills.css'
+import { simpleEvent } from '../utils/digital-analytics'
 
 export default function Tags({ list = [] }) {
   return (
@@ -8,9 +9,8 @@ export default function Tags({ list = [] }) {
       {list.map(tag =>
         <span key={tag}>
           &nbsp;
-          <Link to={`/tags/${tag.toLowerCase()}/`}>
-            <span className="tagPill" data-cat={tag} style={{
-            }}>{tag}</span>
+          <Link to={`/tags/${tag.toLowerCase()}/`} onClick={() => simpleEvent('Tags',tag)}>
+            <span className="tagPill" data-cat={tag}>{tag}</span>
           </Link>
         </span>
       )}
