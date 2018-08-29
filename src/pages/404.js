574bb577287e9f78e605faba289463d52aa7f831
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-08-29 14:13:51 +0200

    Rewriting 404 declaration

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,35 +2,36 @@ import React from 'react'
 import nothing from './404.gif'
 import Link from 'gatsby-link';
 
-const NotFoundPage = () => {
-  // Analytics
-  if (typeof window !== 'undefined') {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      event: 'virtualPageView',
-      pagePath: '/404/',
-      pageTitle: '404 | julienbovet.com'
-    });
-  }
-  //
-  return (
-  <div>
-    <h1>What are you lookin&#39; for? There&#39;s nothing here! </h1>
-    <p>Click on this confused Travolta gif to get back home!</p>
-    <Link  to={`/`}>
-      <img
-        src={nothing}
-        alt={`Nothing!`}
-        // style={{
-        //   marginRight: rhythm(1 / 3),
-        //   marginBottom: 0,
-        //   width: rhythm(2),
-        //   height: rhythm(2),
-        //   borderRadius: 10
-        // }}
-      />
-    </Link>
-  </div>
-)};
+class NotFoundPage extends React.Component {
+  render() {
+    if (typeof window !== 'undefined') {
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: 'virtualPageView',
+        pagePath: '/404/',
+        pageTitle: '404 | julienbovet.com'
+      });
+    }
+    //
+    return (
+    <div>
+      <h1>What are you lookin&#39; for? There&#39;s nothing here! </h1>
+      <p>Click on this confused Travolta gif to get back home!</p>
+      <Link  to={`/`}>
+        <img
+          src={nothing}
+          alt={`Nothing!`}
+          // style={{
+          //   marginRight: rhythm(1 / 3),
+          //   marginBottom: 0,
+          //   width: rhythm(2),
+          //   height: rhythm(2),
+          //   borderRadius: 10
+          // }}
+        />
+      </Link>
+    </div>
+  )};
+}
 
-export default NotFoundPage
+export default NotFoundPage;
