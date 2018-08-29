fd958a5fb1c1b42ea00a1cb8d189df25dd87c318
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-08-29 16:46:01 +0200

    cleaning 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,30 +3,33 @@ import nothing from './404.gif'
 import Link from 'gatsby-link';
 
 class NotFoundPage extends React.Component {
-  componentDidMount(){
-    if (typeof window !== 'undefined') {
-      window.dataLayer = window.dataLayer || [];
-      window.dataLayer.push({
-        event: 'virtualPageView',
-        pagePath: '/404/',
-        pageTitle: '404 | julienbovet.com'
-      });
-    }
-    //
-  }
   render() {
+    console.log("I'm in")
     return (
     <div>
       <h1>What are you lookin&#39; for? There&#39;s nothing here! </h1>
-      <p>Click on this confused Travolta gif to get back home!</p>
-      <Link  to={`/`}>
-        <img
-          src={nothing}
-          alt={`Nothing!`}
-        />
-      </Link>
     </div>
   )};
 }
 
 export default NotFoundPage;
+
+// componentDidMount(){
+//   if (typeof window !== 'undefined') {
+//     window.dataLayer = window.dataLayer || [];
+//     window.dataLayer.push({
+//       event: 'virtualPageView',
+//       pagePath: '/404/',
+//       pageTitle: '404 | julienbovet.com'
+//     });
+//   }
+//   //
+// }
+
+// <p>Click on this confused Travolta gif to get back home!</p>
+// <Link  to={`/`}>
+//   <img
+//     src={nothing}
+//     alt={`Nothing!`}
+//   />
+// </Link>
