a262b623086c050c8099fb600851ad0a0cb32f87
crimsonOrpheo <31985887+crimsonOrpheo@users.noreply.github.com>
2018-08-30 14:12:13 +0200

    Getting back the 404 with correct analytics. cheap fix but still fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import nothing from '../../static/404Travolta.gif'
+import Link from 'gatsby-link'
 
 class NotFoundPage extends React.Component {
   componentDidMount(){
     if (document.getElementsByTagName("h3").length > 0) {
-      console.log("I did mount")
+      if (typeof window !== 'undefined') {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+          event: 'virtualPageView',
+          pagePath: '/404/',
+          pageTitle: '404 | julienbovet.com'
+        });
+      }
     }
   }
   shouldComponentUpdate(){
@@ -14,31 +23,15 @@ class NotFoundPage extends React.Component {
     return (
     <div>
       <h1>What are you lookin&#39; for? There&#39;s nothing here! </h1>
+      <p>Click on this confused Travolta gif to get back home!</p>
+      <Link  to={`/`}>
+        <img
+          src={nothing}
+          alt={`Nothing!`}
+        />
+      </Link>
     </div>
   )};
 }
 
 export default NotFoundPage;
-
-// import nothing from './404.gif'
-// import Link from 'gatsby-link';
-
-// componentDidMount(){
-//   if (typeof window !== 'undefined') {
-//     window.dataLayer = window.dataLayer || [];
-//     window.dataLayer.push({
-//       event: 'virtualPageView',
-//       pagePath: '/404/',
-//       pageTitle: '404 | julienbovet.com'
-//     });
-//   }
-//   //
-// }
-
-// <p>Click on this confused Travolta gif to get back home!</p>
-// <Link  to={`/`}>
-//   <img
-//     src={nothing}
-//     alt={`Nothing!`}
-//   />
-// </Link>
